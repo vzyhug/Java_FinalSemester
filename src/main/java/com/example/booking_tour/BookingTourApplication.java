7e431e264733c233b4b3ce00073f2c69926316e1
Hung <duyhung325tt@gmail.com>
2026-04-12 19:36:07 +0700

    create project

---
+++ src/main/java/com/example/booking_tour/BookingTourApplication.java
@@ -0,0 +1,13 @@
+package com.example.booking_tour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingTourApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookingTourApplication.class, args);
+    }
+
+}
