b17cbf50b1393ee0f9dfd7317023d98362e7266e
Hung <duyhung325tt@gmail.com>
2026-04-12 19:55:23 +0700

    them dependency Web , HomeController

---
+++ src/main/java/com/example/booking_tour/Controller/HomeController.java
@@ -0,0 +1,9 @@
+package com.example.booking_tour.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/home")
+public class HomeController {
+}
