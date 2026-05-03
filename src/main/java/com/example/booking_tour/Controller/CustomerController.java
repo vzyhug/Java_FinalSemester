95cb57b78d1f3fa12b8ad113adec739fc9208c51
Hung <duyhung325tt@gmail.com>
2026-05-03 22:36:09 +0700

    them exception cho thuoc tinh , hoan thien chuc nang DANG KY

--- src/main/java/com/example/booking_tour/Controller/CustomerController.java
+++ src/main/java/com/example/booking_tour/Controller/CustomerController.java
@@ -3,13 +3,25 @@ package com.example.booking_tour.Controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import jakarta.validation.Valid;
 
 import com.example.booking_tour.Model.Customer;
+import com.example.booking_tour.Services.CutomerServices;
 
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
 @Controller
 @RequestMapping("/customer")
+@RequiredArgsConstructor
 public class CustomerController {
+    private final CutomerServices customerServices;
+
     // Hiển thị trang đăng nhập cho khách hàng
     @GetMapping("loginForm")
     public String loginForm(Model model) {
@@ -21,8 +33,19 @@ public class CustomerController {
     // Hiển thị trang đăng ký cho khách hàng
     @GetMapping("registerForm")
     public String registerForm(Model model) {
-        // hiển thị form đăng ký
+        // hiển thị form đăng ky
         model.addAttribute("customer", new Customer());
         return "register_form"; // Trả về tên của view (customer_register.html)
+    }
+
+    // đăng ký tài khoản khách hàng
+    @PostMapping("register")
+    public String register(@Valid @ModelAttribute("customer") Customer customer, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "register_form"; // Trả lại form nếu có lỗi
+        }
+        // Lưu thông tin vào database
+        customerServices.register(customer);
+        return "redirect:/customer/loginForm";
     }
 }
--- src/main/java/com/example/booking_tour/Services/CutomerServices.java
+++ src/main/java/com/example/booking_tour/Services/CutomerServices.java
@@ -2,6 +2,23 @@ package com.example.booking_tour.Services;
 
 import org.springframework.stereotype.Service;
 
+import com.example.booking_tour.Model.Customer;
+import com.example.booking_tour.Repository.CustomerRepository;
+
+import jakarta.transaction.Transactional;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
 @Service
+@RequiredArgsConstructor
 public class CutomerServices {
+    private final CustomerRepository customerRepository;
+
+    // Lưu thông tin khách hàng mới sau khi đăng ký
+    @Transactional
+    public void register(Customer customer) {
+        // Lưu thông tin vào database
+        customerRepository.save(customer);
+    }
 }
